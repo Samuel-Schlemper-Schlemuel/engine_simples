a96bab883b2fda9cb16471784b381938a5078158
Samuel Schlemper <samuelschlemper2006@gmail.com>
2022-09-20 00:08:33 -0300

    Pequenas alterações

--- README.md
+++ README.md
@@ -7,36 +7,40 @@ Ele será criado com HTML, CSS e JavaScript.
 
 Como nunca criei um projeto desse tipo antes, ele irá demorar muito tempo para ficar pronto, eu possivelmente farei outros projetos nesse meio tempo com assuntos que estarei estudando para fazer esse, talvez até desista desse daqui no meio do caminho.
 
-## Dia 1
+## Atualização 1
 Já adicionei um modo de criar fases novas e perguntas novas.
 
-## Dia 2
+## Atualização 2
 Adicionei um modo que limita a quantidade maxima de caixas de texto e um botão que apaga a ultima caixa, deixei como minimo duas caixas em vez de quatro como na versão anterior.
 Adicionei um botão que remove a última fase, deixando um minimo de uma fase.
 
-## Dia 3
-Não consegui criar muita coisa hoje, apenas adicionei um botão de criar fase, mudei algumas coisas no CSS e criar a função criar jogo.
+## Atualização 3
+Adicionei um botão de criar fase, mudei algumas coisas no CSS e criei a função criar jogo.
 
-## Dia 4
+## Atualização 4
 Mudei o nome da função criar_jogo() para function manipular_perguntas() e agora quando se cria uma fase ou pergunta nova as que já estavam escritas não se apagam.
 
-## Dia 5
+## Atualização 5
 Havia um problema de lógica no HTML que fazia com que toda vez que eu adicionasse uma pergunta, ela fosse para baixo do botão 'criar jogo!', mas agora o problema foi resolvido.
 Finalmente posso dizer que meu site cria um jogo, por mais que ele é bem mal feito e só pode ser usado localmente enquanto o site estiver aberto.
 
-## Dia 6
+## Atualização 6
 Percebi que havia um erro no jogo que quando a pessoa escreve as possiveis respostas não a como escrever a pergunta, então adicionei essa opção.
-Agora da para ver quantos acertos ouve e os botões se tornaram responsiveis e estão todos de tamanho igual agora.
+Agora da para ver quantos acertos houve e os botões se tornaram responsiveis e estão todos de tamanho igual agora.
 Agora em vez de dar um alert toda ves que acerta ou erra, o código bota uma tag h1 por 2 segundos dizendo se ouve um acerto ou erro.
 
-## Dia 7
+## Atualização 7
 Agora o resultado (acertou/errou) e o final estão centralizados
-Amanhã começa uma das partes mais complexas do projeto, que é o fato de que eu começarei a usar funções das quais não tenho nenhum conhecimento ainda, que é algo que não aconteceu até o momento atual, o primeiro objetivo será criar algo que deixe fácil para o usuário mudar a cor de fundo.
+Começarei uma das partes mais complexas do projeto, que é o fato de que eu começarei a usar funções das quais não tenho nenhum conhecimento ainda, que é algo que não aconteceu até o momento atual, o primeiro objetivo será criar algo que deixe fácil para o usuário mudar a cor de fundo.
 
-## Dia 8
+## Atualização 8
 Descobri a existencia de um input que tem como valor type="color", ultilizarei isso para criar um seletor de cores para meu usuário.
 Input pronto e mudando a cor, agora o trabalho é botar o site no ar.
 Acabei de fazer com que o código tenha um site hospedado no próprio github: [Engine Simples](https://samuel-schlemper-schlemuel.github.io/engine_simples/Home.html).
 
-## Dia 9
+## Atualização 9
 Agora os botões são centralizados caso não haja imagem.
+
+## Atualização 10
+Depois de muito tempo estou retornando a esse projeto. A atualização dessa vez é pequena só organizei um pouco o código e adicionei a 
+possibilidade de mudar a cor das palavras acerto e erro.
--- js.js
+++ js.js
@@ -5,30 +5,42 @@ var perguntas_da_fase = []
 var contagem_de_arrays = 0
 var pontuacao = 0
 var imagem = false
+var cor_acerto = 'green'
+var cor_erro = 'red'
 
 function adicionar_fase(){
     manipular_perguntas()
     document.getElementById('perguntas').innerHTML += `<div id='fase_completa_${quantidade_fase + 1}'>
-                                                         <p>&nbsp&nbsp&nbsp&nbspFase ${quantidade_fase + 1}: Clique para adicionar uma nova pergunta a essa fase (minimo de 2 e maximo de 8)
+                                                         <p>${repeat('&nbsp', 4)}Fase ${quantidade_fase + 1}: Clique para adicionar uma nova pergunta a essa fase (minimo de 2 e maximo de 8)
                                                             <button id='nova_pergunta' onclick="adicionar_pergunta(${quantidade_fase + 1})">Nova Pergunta</button> 
                                                             <button id='deletar_pergunta' onclick="deletar_pergunta(${quantidade_fase + 1})">Deletar última pergunta</button></p>
                                                             <div id='fase_${quantidade_fase + 1}'>
-                                                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input id='fase_${quantidade_fase + 1}_pergunta_1' type="text" placeholder="A pergunta">
-                                                               <div id='input_1_fase_${quantidade_fase + 1}'> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input id='fase_${quantidade_fase + 1}_pergunta_2' type="text" placeholder="A resposta certa"> </div>
-                                                                <div id='input_2_fase_${quantidade_fase + 1}'> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input id='fase_${quantidade_fase + 1}_pergunta_3' type="text" placeholder="Uma das respostas erradas"> </div>
+                                                            ${repeat('&nbsp', 8)}<input id='fase_${quantidade_fase + 1}_pergunta_1' type="text" placeholder="A pergunta">
+                                                               <div id='input_1_fase_${quantidade_fase + 1}'> ${repeat('&nbsp', 8)}<input id='fase_${quantidade_fase + 1}_pergunta_2' type="text" placeholder="A resposta certa"> </div>
+                                                                <div id='input_2_fase_${quantidade_fase + 1}'> ${repeat('&nbsp', 8)}<input id='fase_${quantidade_fase + 1}_pergunta_3' type="text" placeholder="Uma das respostas erradas"> </div>
                                                             </div>
-                                                        </div>      `
+                                                        </div>`
     valor()
-    quantidade_fase++
+    quantidade_fase += 1
     fases[quantidade_fase] = 3
+}
+
+function repeat(text, times){
+    result = ''
+
+    for(i = 0; i < times; i++){
+        result += text
     }
+    
+    return result
+}
 
 function remover_fase(){
     if(quantidade_fase === 1){
         alert('Minimo de fases alcançado')
     } else {
     document.getElementById(`fase_completa_${quantidade_fase}`).remove()
-    quantidade_fase--
+    quantidade_fase -= 1
     }
 }
 
@@ -37,7 +49,7 @@ function adicionar_pergunta(fase){
         alert('Limite maximo de escolhas alcançado na fase ' + fase)
     } else {
         manipular_perguntas()
-        document.getElementById(`fase_${fase}`).innerHTML += `<div id='input_${fases[fase] + 1}_fase_${fase}'> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input id='fase_${fase}_pergunta_${fases[fase] + 1}' type="text" placeholder="Uma das respostas erradas"> </div>`
+        document.getElementById(`fase_${fase}`).innerHTML += `<div id='input_${fases[fase] + 1}_fase_${fase}'> ${repeat('&nbsp', 8)}<input id='fase_${fase}_pergunta_${fases[fase] + 1}' type="text" placeholder="Uma das respostas erradas"> </div>`
         valor()
         fases[fase] += 1
     }
@@ -82,12 +94,8 @@ function criar_jogo(){
 
 function botoes(){
     let ordenado = perguntas[contagem_de_arrays].slice(1,)
-    let tamanho_das_perguntas = 0
-    for(let i = 0; i < ordenado.length; i++){
-        tamanho_das_perguntas += ordenado[i].length
-    }
     ordenado.sort()
-    tela.innerHTML = perguntas[contagem_de_arrays][0] + ':<br> <div id="botoes_tela"><div>'
+    tela.innerHTML = perguntas[contagem_de_arrays][0] + '<br> <div id="botoes_tela"><div>'
     var botoes_tela = document.getElementById('botoes_tela')
     for(let c = 0; c < ordenado.length; c++){
         if(perguntas[contagem_de_arrays][1] === ordenado[c]){
@@ -101,16 +109,16 @@ function botoes(){
     } else {
         document.getElementById('botoes_tela').style.paddingLeft = '150px'
     }
-    contagem_de_arrays++
+    contagem_de_arrays += 1
 }
 
 function resposta(classe){
     tela.style.textAlign = 'center'
     if(classe === 'certo'){
-        tela.innerHTML = `<h1 style='color: green;'>Acertou</h1>`
+        tela.innerHTML = `<h1 style='color: ${cor_acerto};'>Acertou</h1>`
         pontuacao++
     } else {
-        tela.innerHTML = `<h1 style='color: red;'>Errou</h1>`
+        tela.innerHTML = `<h1 style='color: ${cor_erro};'>Errou</h1>`
     }
     setTimeout(() => {
         if(contagem_de_arrays === perguntas.length){
@@ -119,10 +127,20 @@ function resposta(classe){
             tela.style.textAlign = 'left'
             botoes()
         }
-    }, 2000);
+    }, 1500);
 }
 
-function mudar_cor(){
-    let cor = document.getElementById('seletor_cores').value
+function mudar_cor_fundo(){
+    let cor = document.getElementById('seletor_cores_fundo').value
     document.getElementById('tela').style.backgroundColor = cor
+}
+
+function mudar_cor_acerto(){
+    let cor = document.getElementById('seletor_cores_acerto').value
+    cor_acerto = cor
+}
+
+function mudar_cor_erro(){
+    let cor = document.getElementById('seletor_cores_erro').value
+    cor_erro = cor
 }
